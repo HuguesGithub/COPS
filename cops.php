04b1f5ae8a06b0de95438e5871332242fa2b2f30
JONEAUX Hugues <jhugues@jhugues.fr>
2023-03-12 15:17:22 +0100

    Prise en compte renommage et gestion erreurs

--- cops.php
+++ cops.php
@@ -20,7 +20,7 @@ class Cops
   public function template_loader()
   {
     wp_enqueue_script('jquery');
-    return PLUGIN_PATH.'web/pages/public/main-public-page.php';
+    return PLUGIN_PATH.'web/pages/publique/main-publique-page.php';
   }
 }
 $Cops = new Cops();
@@ -34,36 +34,28 @@ $Cops = new Cops();
 spl_autoload_register(PLUGIN_PACKAGE.'_autoloader');
 function cops_autoloader($classname)
 {
-  $pattern = "/(Bean|DaoImpl|Dao|Services|Actions|Utils|Interface)/";
-  preg_match($pattern, $classname, $matches);
-  if (isset($matches[1])) {
-    switch ($matches[1]) {
-      case 'Actions' :
-      case 'Bean' :
-      case 'Dao' :
-      case 'DaoImpl' :
-      case 'Interface' :
-      case 'Services' :
-      case 'Utils' :
-        if (file_exists(PLUGIN_PATH.'core/'.strtolower($matches[1]).'/'.$classname.'.php')) {
-          include_once(PLUGIN_PATH.'core/'.strtolower($matches[1]).'/'.$classname.'.php');
-        } elseif (file_exists(PLUGIN_PATH.'../mycommon/core/'.strtolower($matches[1]).'/'.$classname.'.php')) {
-          include_once(PLUGIN_PATH.'../mycommon/core/'.strtolower($matches[1]).'/'.$classname.'.php');
+    $matches = array();
+    $arr = array(
+        'Actions' => 'actions',
+        'Bean' => 'bean',
+        'Class' => 'domain',
+        'DaoImpl' => 'daoimpl',
+        'Interface' => 'interfaceimpl',
+        'Services' => 'services',
+    );
+    $pattern = "/(Actions|Bean|Class|DaoImpl|Interface|Services)/";
+    if (preg_match($pattern, $classname, $matches)) {
+        if (strpos($classname, '\\')!==false) {
+            $classname = substr($classname, strrpos($classname, '\\')+1);
         }
-      break;
-      default :
-        // On est dans un cas o? on a match? mais pas pr?vu le traitement...
-      break;
+
+        if (isset($arr[$matches[1]])) {
+            $filePath = PLUGIN_PATH.'core/'.$arr[$matches[1]].'/'.$classname.'.php';
+            if (file_exists($filePath)) {
+                include_once($filePath);
+            }
+        }
     }
-  } else {
-    $classfile = sprintf('%score/domain/%s.class.php', PLUGIN_PATH, str_replace('_', '-', $classname));
-    if (!file_exists($classfile)) {
-      $classfile = sprintf('%s../mycommon/core/domain/%s.class.php', PLUGIN_PATH, str_replace('_', '-', $classname));
-    }
-    if (file_exists($classfile)) {
-      include_once($classfile);
-    }
-  }
 }
 
 /**
@@ -106,3 +98,38 @@ function dealWithAjax_callback()
   echo AjaxActions::dealWithAjax();
   die();
 }
+
+/**
+#######################################################################################
+### Gestion des Exceptions
+### Description: Met en forme les exceptions
+#######################################################################################
+*/
+function exception_handler($objException)
+{
+  echo
+    '<div class="card border-danger" style="max-width: 100%;margin-right: 15px;">'.
+    '  <div class="card-header bg-danger text-white"><strong>'.$objException->getMessage().'</strong></div>'.
+    '  <div class="card-body text-danger">'.
+    '    <p>Une erreur est survenue dans le fichier <strong>'.$objException->getFile().
+    '</strong> à la ligne <strong>'.$objException->getLine().'</strong>.</p>'.
+    '    <ul class="list-group">';
+
+  $arrTraces = $objException->getTrace();
+    foreach ($arrTraces as $trace) {
+        echo '<li class="list-group-item">Fichier <strong>'.$trace['file'];
+        echo '</strong> ligne <em>'.$trace['line'].'</em> :<br>';
+        if (is_array($trace['args'])) {
+            echo $trace['function'].'()</li>';
+        } else {
+            echo $trace['class'].$trace['type'].$trace['function'].'('.implode(', ', $trace['args']).')</li>';
+        }
+    }
+
+  echo
+    '    </ul>'.
+    '  </div>'.
+    '  <div class="card-footer"></div>'.
+    '</div>';
+}
+set_exception_handler('exception_handler');
